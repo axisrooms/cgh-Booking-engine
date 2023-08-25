d6c6bc03ba54739294b6872d17cedbd65787318f
Singanamala Sharukh Basha <singanamala.sharukh@axisrooms.com>
2023-08-25 09:00:11 +0530

    add null check

--- src/app/shared/components/room/room.component.ts
+++ src/app/shared/components/room/room.component.ts
@@ -57,7 +57,7 @@ data:any|null;
   onBookNow() {
     this.data =  localStorage.getItem('reflectStore');
     this.data =JSON.parse(this.data);
-    if(this.data.length == 0 && this.flag){
+    if(!this.data && this.flag){
     
 
       this.flag = false
