8fc066535721e53b347725b5f29adcbec454dd85
Singanamala Sharukh Basha <singanamala.sharukh@axisrooms.com>
2025-04-20 13:33:50 +0530

    v5 BEID changes

--- src/app/services/bookingid.service.ts
+++ src/app/services/bookingid.service.ts
@@ -7,18 +7,18 @@ import { ActivatedRoute } from '@angular/router';
 export class BookingConfigService {
   bookingEngineId: number | undefined;
 
-  constructor(private route: ActivatedRoute) {
-    this.route.paramMap.subscribe(params => {
-      const hotelId = params.get('hotelId'); // Make sure it's 'hotelId' (case-sensitive)
-      console.log('Hotel ID from paramMap:', hotelId);
-      
-      if (hotelId) {
-        this.bookingEngineId = +hotelId; // Convert to number
-        console.log('Updated Booking Engine ID:', this.bookingEngineId);
-      }
-    });
-  }
 
+constructor(private route: ActivatedRoute) {
+  this.route.queryParamMap.subscribe(params => {
+    const hotelId = params.get('hotelId'); // Now reads from query params
+    console.log('Hotel ID from queryParams:', hotelId);
+    
+    if (hotelId) {
+      this.bookingEngineId = +hotelId; // Convert to number
+      console.log('Updated Booking Engine ID:', this.bookingEngineId);
+    }
+  });
+}
   getBookingEngineId() {
     return Number(this.bookingEngineId);
   }
